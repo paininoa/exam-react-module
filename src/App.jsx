335235b0eecdb0f523df14f35b30f740fb70cf9a
Alessio <carninoalessio@gmail.com>
2023-12-21 10:19:35 +0100

    Ex 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./App.scss";
+import About from "./Components/About";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>React module exam</h1>
+      <About />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/About.jsx
@@ -0,0 +1,8 @@
+export default () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This app has been created for the React module exam.</p>
+    </div>
+  );
+};
